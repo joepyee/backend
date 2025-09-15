7572d86ced023059c3ebdfe17678d05d53178c12
Joe Riegsecker <jriegsecker@ivytech.edu>
2025-09-15 16:16:41 -0400

    created first api route to get songs

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
 const express = require("express");
+var cors = require("cors");
 const app = express();
+app.use(cors());
+const router = express.Router();
 
-//start web server
-app.listen(3000, function () {
-  console.log("Listening on port 3000");
-});
 //making api using routes. Look like urls
 //GET request to http://localhost:3000/hello
+
+router.get("/songs", function (req, res) {
+  const songs = [
+    {
+      title: "We Found Love",
+      artist: "Rihanna",
+      popularity: 10,
+      releaseDate: new Date(2011, 9, 22),
+      genre: ["electro house"],
+    },
+    {
+      title: "Happy",
+      artist: "Pharrell Williams",
+      popularity: 10,
+      releaseDate: new Date(2013, 11, 21),
+      genre: ["soul", "new soul"],
+    },
+  ];
+
+  res.json(songs);
+});
+//all request that usually use an api statrt with /api localhost:3000/api/songs
+app.use("/api", router);
+app.listen(3000);
+console.log("listening");
